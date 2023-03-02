8285362758037e3b64e0537a31a52e14ce504b75
ruchita0 <ruchitamangalaram@gmail.com>
2023-03-03 02:48:12 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -101,9 +101,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -121,8 +120,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
               child: Text(
                 'False',
                 style: TextStyle(
